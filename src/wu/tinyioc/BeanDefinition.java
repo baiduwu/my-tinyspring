9718f6e29b57539adbcc4e7ac909821780ebffb5
wuzhiqiang <wuzhiqiang@rongyi.com>
2016-06-03 21:14:33 +0800

    中文注释

--- src/wu/tinyioc/BeanDefinition.java
+++ src/wu/tinyioc/BeanDefinition.java
@@ -1,15 +1,15 @@
 package wu.tinyioc;
 
 /**
- * desc 
+ * 
  * @author wuzhiqiang
- * @date 2016-6-2 下午10:39:59
+ *
  */
 public class BeanDefinition {
   private Object bean;
   
   /**
-   * desc 构造方法
+   * 
    * @param bean
    */
   public BeanDefinition(Object bean){
